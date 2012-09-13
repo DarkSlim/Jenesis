f55777201bf6008b728208b7268a7c15335da782
William Best <wmbest2@gmail.com>
2012-09-13 13:06:21 -0500

    Removed SUBQ

--- search/all_69.js
+++ search/all_69.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['immediateinstruction',['ImmediateInstruction',['../classcom_1_1wmbest_1_1jenesis_1_1m68k_1_1instructions_1_1ImmediateInstruction.html',1,'com::wmbest::jenesis::m68k::instructions']]]
+  ['immediateinstruction',['ImmediateInstruction',['../classcom_1_1wmbest_1_1jenesis_1_1m68k_1_1instructions_1_1ImmediateInstruction.html',1,'com::wmbest::jenesis::m68k::instructions']]],
+  ['instruction',['Instruction',['../classcom_1_1wmbest_1_1jenesis_1_1m68k_1_1instructions_1_1Instruction.html',1,'com::wmbest::jenesis::m68k::instructions']]]
 ];
