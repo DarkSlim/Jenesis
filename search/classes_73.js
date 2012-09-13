9351ffead5fa3739e42a81aba403286511853ff1
William Best <wmbest2@gmail.com>
2012-09-13 12:59:06 -0500

    Added more todos and empty classes

--- search/classes_73.js
+++ search/classes_73.js
@@ -3,5 +3,6 @@ var searchData=
   ['seainstruction',['SEAInstruction',['../classcom_1_1wmbest_1_1jenesis_1_1m68k_1_1instructions_1_1SEAInstruction.html',1,'com::wmbest::jenesis::m68k::instructions']]],
   ['sixtyeightk',['SixtyEightK',['../classcom_1_1wmbest_1_1jenesis_1_1m68k_1_1SixtyEightK.html',1,'com::wmbest::jenesis::m68k']]],
   ['subi',['SubI',['../classcom_1_1wmbest_1_1jenesis_1_1m68k_1_1instructions_1_1SubI.html',1,'com::wmbest::jenesis::m68k::instructions']]],
+  ['subq',['SubQ',['../classcom_1_1wmbest_1_1jenesis_1_1m68k_1_1instructions_1_1SubQ.html',1,'com::wmbest::jenesis::m68k::instructions']]],
   ['systeminstruction',['SystemInstruction',['../classcom_1_1wmbest_1_1jenesis_1_1m68k_1_1instructions_1_1SystemInstruction.html',1,'com::wmbest::jenesis::m68k::instructions']]]
 ];
