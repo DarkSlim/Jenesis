230c85194e6795be26e4b144b8cf9c776c379821
William Best <wmbest2@gmail.com>
2012-09-13 20:55:22 -0500

    Added more branching code

--- search/all_73.js
+++ search/all_73.js
@@ -1,5 +1,6 @@
 var searchData=
 [
+  ['scc',['Scc',['../classcom_1_1wmbest_1_1jenesis_1_1m68k_1_1instructions_1_1Scc.html',1,'com::wmbest::jenesis::m68k::instructions']]],
   ['seainstruction',['SEAInstruction',['../classcom_1_1wmbest_1_1jenesis_1_1m68k_1_1instructions_1_1SEAInstruction.html',1,'com::wmbest::jenesis::m68k::instructions']]],
   ['setval',['setVal',['../classcom_1_1wmbest_1_1jenesis_1_1m68k_1_1Operand.html#a0216c18d36ae0ec59bd4e9b79294856f',1,'com::wmbest::jenesis::m68k::Operand']]],
   ['sixtyeightk',['SixtyEightK',['../classcom_1_1wmbest_1_1jenesis_1_1m68k_1_1SixtyEightK.html',1,'com::wmbest::jenesis::m68k']]],
--- search/classes_73.js
+++ search/classes_73.js
@@ -1,5 +1,6 @@
 var searchData=
 [
+  ['scc',['Scc',['../classcom_1_1wmbest_1_1jenesis_1_1m68k_1_1instructions_1_1Scc.html',1,'com::wmbest::jenesis::m68k::instructions']]],
   ['seainstruction',['SEAInstruction',['../classcom_1_1wmbest_1_1jenesis_1_1m68k_1_1instructions_1_1SEAInstruction.html',1,'com::wmbest::jenesis::m68k::instructions']]],
   ['sixtyeightk',['SixtyEightK',['../classcom_1_1wmbest_1_1jenesis_1_1m68k_1_1SixtyEightK.html',1,'com::wmbest::jenesis::m68k']]],
   ['subi',['SubI',['../classcom_1_1wmbest_1_1jenesis_1_1m68k_1_1instructions_1_1SubI.html',1,'com::wmbest::jenesis::m68k::instructions']]],
