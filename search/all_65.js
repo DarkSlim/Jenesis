2d6e0e4892bb5974cad7704ce9821d01ebcc191e
William Best <wmbest2@gmail.com>
2012-09-14 15:41:31 -0500

    Added more Immediate instructions

---
+++ search/all_65.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['eori',['EorI',['../classcom_1_1wmbest_1_1jenesis_1_1m68k_1_1instructions_1_1EorI.html',1,'com::wmbest::jenesis::m68k::instructions']]],
+  ['eoritoccr',['EORItoCCR',['../classcom_1_1wmbest_1_1jenesis_1_1m68k_1_1instructions_1_1EORItoCCR.html',1,'com::wmbest::jenesis::m68k::instructions']]]
+];
--- search/all_6f.js
+++ search/all_6f.js
@@ -1,4 +1,6 @@
 var searchData=
 [
-  ['operand',['Operand',['../classcom_1_1wmbest_1_1jenesis_1_1m68k_1_1Operand.html',1,'com::wmbest::jenesis::m68k']]]
+  ['operand',['Operand',['../classcom_1_1wmbest_1_1jenesis_1_1m68k_1_1Operand.html',1,'com::wmbest::jenesis::m68k']]],
+  ['ori',['OrI',['../classcom_1_1wmbest_1_1jenesis_1_1m68k_1_1instructions_1_1OrI.html',1,'com::wmbest::jenesis::m68k::instructions']]],
+  ['oritoccr',['ORItoCCR',['../classcom_1_1wmbest_1_1jenesis_1_1m68k_1_1instructions_1_1ORItoCCR.html',1,'com::wmbest::jenesis::m68k::instructions']]]
 ];
