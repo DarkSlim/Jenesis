25563a6fe6ad4c50aef63396c01981f9225fc405
William Best <wmbest2@gmail.com>
2012-09-13 20:41:07 -0500

    Updated for added functions

--- search/classes_61.js
+++ search/classes_61.js
@@ -5,5 +5,7 @@ var searchData=
   ['addq',['AddQ',['../classcom_1_1wmbest_1_1jenesis_1_1m68k_1_1instructions_1_1AddQ.html',1,'com::wmbest::jenesis::m68k::instructions']]],
   ['addressable',['Addressable',['../interfacecom_1_1wmbest_1_1jenesis_1_1util_1_1Addressable.html',1,'com::wmbest::jenesis::util']]],
   ['and',['And',['../classcom_1_1wmbest_1_1jenesis_1_1m68k_1_1instructions_1_1And.html',1,'com::wmbest::jenesis::m68k::instructions']]],
+  ['andi',['AndI',['../classcom_1_1wmbest_1_1jenesis_1_1m68k_1_1instructions_1_1AndI.html',1,'com::wmbest::jenesis::m68k::instructions']]],
+  ['anditoccr',['ANDItoCCR',['../classcom_1_1wmbest_1_1jenesis_1_1m68k_1_1instructions_1_1ANDItoCCR.html',1,'com::wmbest::jenesis::m68k::instructions']]],
   ['app',['App',['../classcom_1_1wmbest_1_1jenesis_1_1App.html',1,'com::wmbest::jenesis']]]
 ];
--- search/classes_62.js
+++ search/classes_62.js
@@ -1,4 +1,5 @@
 var searchData=
 [
+  ['bcc',['Bcc',['../classcom_1_1wmbest_1_1jenesis_1_1m68k_1_1instructions_1_1Bcc.html',1,'com::wmbest::jenesis::m68k::instructions']]],
   ['bitwiseinstruction',['BitwiseInstruction',['../classcom_1_1wmbest_1_1jenesis_1_1m68k_1_1instructions_1_1BitwiseInstruction.html',1,'com::wmbest::jenesis::m68k::instructions']]]
 ];
