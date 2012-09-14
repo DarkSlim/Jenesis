52e7cd252c0c1382e41c4736247ca3704e036158
William Best <wmbest2@gmail.com>
2012-09-14 13:13:11 -0500

    Updated to include bitwise operations

--- search/all_62.js
+++ search/all_62.js
@@ -1,5 +1,9 @@
 var searchData=
 [
   ['bcc',['Bcc',['../classcom_1_1wmbest_1_1jenesis_1_1m68k_1_1instructions_1_1Bcc.html',1,'com::wmbest::jenesis::m68k::instructions']]],
-  ['bitwiseinstruction',['BitwiseInstruction',['../classcom_1_1wmbest_1_1jenesis_1_1m68k_1_1instructions_1_1BitwiseInstruction.html',1,'com::wmbest::jenesis::m68k::instructions']]]
+  ['bchg',['Bchg',['../classcom_1_1wmbest_1_1jenesis_1_1m68k_1_1instructions_1_1Bchg.html',1,'com::wmbest::jenesis::m68k::instructions']]],
+  ['bclr',['Bclr',['../classcom_1_1wmbest_1_1jenesis_1_1m68k_1_1instructions_1_1Bclr.html',1,'com::wmbest::jenesis::m68k::instructions']]],
+  ['bitwiseinstruction',['BitwiseInstruction',['../classcom_1_1wmbest_1_1jenesis_1_1m68k_1_1instructions_1_1BitwiseInstruction.html',1,'com::wmbest::jenesis::m68k::instructions']]],
+  ['bset',['Bset',['../classcom_1_1wmbest_1_1jenesis_1_1m68k_1_1instructions_1_1Bset.html',1,'com::wmbest::jenesis::m68k::instructions']]],
+  ['btst',['Btst',['../classcom_1_1wmbest_1_1jenesis_1_1m68k_1_1instructions_1_1Btst.html',1,'com::wmbest::jenesis::m68k::instructions']]]
 ];
